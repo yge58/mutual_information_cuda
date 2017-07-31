b821a583a160e7f6568c77acc0991d5a56ae5992
yge58 <yge58@uwo.ca>
2017-07-31 09:09:52 -0400

    build mattes mutual info

---
+++ sample_build_MattesMutualInformation/CMakeFiles/mattesMutualInformation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mattesMutualInformation.dir/mattesMutualInformation_generated_cudaMattesMutualInformation64bin.cu.o"
+  "CMakeFiles/mattesMutualInformation.dir/itkMattesMutualInformation.cxx.o"
+  "CMakeFiles/mattesMutualInformation.dir/cudaMattesMutualInformation.cxx.o"
+  "CMakeFiles/mattesMutualInformation.dir/main.cpp.o"
+  "mattesMutualInformation.pdb"
+  "mattesMutualInformation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mattesMutualInformation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
